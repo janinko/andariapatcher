1182ee2a869baa255b8f1de546a55ce08708f9b0
p0l0us <p0l0us@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-05-14 15:28:37 +0000

    import existujicich funkci pro runner v c++

---
+++ AndariaRunner/process.h
@@ -0,0 +1,11 @@
+#include <windows.h>
+/*
+#include <stdio.h>
+#include <tchar.h>
+#include <psapi.h>
+#include <iostream>
+//#include <cstdlib>
+//#include <Tlhelp32.h>
+//*/
+int getRunningProcess();
+void PrintProcessNameAndID( DWORD processID );

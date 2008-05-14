66242ea2be9da01cecaf4f761eda8681a185abd3
p0l0us <p0l0us@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-05-14 15:30:44 +0000

    Moved remotely

--- AndariaRunner/macId.h
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include <Iphlpapi.h>
-#include <Assert.h>
-#pragma comment(lib, "iphlpapi.lib")
-
-
-DWORD GetMACaddress(void);
-//static void PrintMACaddress(unsigned char MACData[]);

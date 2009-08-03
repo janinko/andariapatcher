8c5c74a52094fd4e75cd062c14daf9f0948e32dc
p0l0us <p0l0us@843fd666-81f2-4710-8602-7759cdb5ad05>
2009-08-03 18:20:36 +0000

    konec unraru - odstraneny vsechny zminky #33

--- trunk/AndariaPatcher/src/cz/polous/andaria/LinuxOS.java
+++ trunk/AndariaPatcher/src/cz/polous/andaria/LinuxOS.java
@@ -8,7 +8,7 @@ import java.io.File;
  ******************************************************************************/
 class LinuxOS extends OperatingSystem {
 
-    private final String unrar_path = "unrar";
+  
     private final String ultima_online_path = System.getProperty("user.home") + "/.wine/drive_c/Program Files/EA Games/Ultima Online Mondain's Legacy";
     private final String run_command = "wine " + ultima_online_path + "/AndariaClient.exe";
 
@@ -21,14 +21,6 @@ class LinuxOS extends OperatingSystem {
     String getUltima_online_path() {
         return ultima_online_path;
     }
-    /*@Override
-    String getUnrar_path() {
-    return unrar_path;
-    }*/
-
-    /*@Override
-    public void downloadUnrar() {
-    }*/
 
     /** Creates a new instance of LinuxOS
      **************************************************************************/
@@ -51,19 +43,4 @@ class LinuxOS extends OperatingSystem {
     public String[] getBatchExecCommand(File f) {
         return new String[]{"wine", "cmd", "/C ", f.getName()};
     }
-
-    /***************************************************************************
-     * TODO: Java unrar implementation (using unrar native library).
-     * @param file to extract
-     **************************************************************************/
-    /*@Override
-    public void unrar(File file) {
-    }*/
-
-    /***************************************************************************
-     * TODO: Unrar library inicialization
-     **************************************************************************/
-    /*@Override
-    public void unrarInit() {
-    }*/
 }
